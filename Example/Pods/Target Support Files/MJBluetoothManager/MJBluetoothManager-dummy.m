4726851acd59a498267e17f1124fc9eb0704c702
Paolo Tagliani <paolo@mobilejazz.com>
2016-11-23 10:33:31 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/MJBluetoothManager/MJBluetoothManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MJBluetoothManager : NSObject
+@end
+@implementation PodsDummy_MJBluetoothManager
+@end
---
+++ Example/Pods/Target Support Files/Pods-MJBluetoothManager_Tests/Pods-MJBluetoothManager_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MJBluetoothManager_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MJBluetoothManager_TestsVersionString[];
+
