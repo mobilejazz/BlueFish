c4f1870c811fb5517d877f59a1ad84f021d66686
Paolo Tagliani <paolo@mobilejazz.com>
2016-12-05 17:00:06 +0100

    Added BlueFish.h file to include all headers

--- BlueFish/Classes/BlueFish.h
+++ BlueFish/Classes/BlueFish.h
@@ -1,13 +1,18 @@
 //
-//  BlueFish.h
-//  Pods
+// Copyright 2016 Mobile Jazz SL
 //
-//  Created by Paolo Tagliani on 05/12/16.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
-#ifndef BlueFish_h
-#define BlueFish_h
-
-
-#endif /* BlueFish_h */
+#import "BFCentralManager.h"
+#import "BFPeripheral.h"
